4050c9828f1ffaa2061b6b1fcba9ce43fdfe8b2f
Noah Solnick <solnick.noah@gmail.com>
2023-02-15 12:11:28 -0500

    Add new sketchpad and clear sketchpad functions - finished project

--- eas.js
+++ eas.js
@@ -1,22 +1,48 @@
 let container = document.getElementById("container");
 
-function makeRows (rows, columns) {
+function makeGrid (rows, columns) {
+    error.textContent = "";
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-columns", columns);
-    for (c = 0; c < (rows * columns); c++) {
+    let gridSize = rows * columns
+    for (c = 0; c < (gridSize); c++) {
         let cell = document.createElement("div");
+        cell.id = c;
         cell.addEventListener("mouseover",() => {
             cell.style.backgroundColor = "black"
         });
         container.appendChild(cell).className = "cell";
     };
-}
+};
 
-// function fillCell(e) {
-//     e.target.style = `background-color: black`;
+function wipeGrid() {
+    let cells = container.querySelectorAll("div")
+    cells.forEach((div) => div.remove());
+};
 
-    // const gridItems = document.querySelectorAll(".cell");
-    // gridItems.forEach(cell => cell.addEventListener("mouseover", () => fillCell()))
-makeRows(16,16);
+let create = document.getElementById("create");
+let clear = document.getElementById("clear");
 
-    
+create.addEventListener("click", () => {
+    let gridSize = prompt("How big is the grid (2 - 100)?");
+    wipeGrid();
+    makeGrid(gridSize, gridSize);
+    throwError();
+});
+
+clear.addEventListener("click", () => {
+    wipeGrid();
+    makeGrid(16, 16);
+});
+
+makeGrid(16,16);
+
+function throwError() {
+    const cells = document.getElementById("container").childElementCount;
+    let gridSize = Math.sqrt(cells);
+    if (gridSize < 2 || gridSize > 100) {
+        wipeGrid();
+        let error = document.getElementById("error");
+        error.textContent = "Sorry wrong size try again";
+    };
+};
